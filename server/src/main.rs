7efe07da929a419265e56bd075092ef6e55b00a7
Pixelprizm <pixelprizm@gmail.com>
2024-06-28 23:40:37 -0700

    remove examples

--- server/src/main.rs
+++ server/src/main.rs
@@ -118,33 +118,7 @@ fn main() {
         c.set_color(1., 0.8, 0.2);
     });
 
-    let mut c = window.add_cube(1.0, 1.0, 1.0);
-    let mut s = window.add_sphere(0.5);
-    let mut p = window.add_cone(0.5, 1.0);
-    let mut y = window.add_cylinder(0.5, 1.0);
-    let mut a = window.add_capsule(0.5, 1.0);
-
-    c.set_color(random(), random(), random());
-    s.set_color(random(), random(), random());
-    p.set_color(random(), random(), random());
-    y.set_color(random(), random(), random());
-    a.set_color(random(), random(), random());
-
-    c.append_translation(&Translation3::new(2.0, 0.0, 0.0));
-    s.append_translation(&Translation3::new(4.0, 0.0, 0.0));
-    p.append_translation(&Translation3::new(-2.0, 0.0, 0.0));
-    y.append_translation(&Translation3::new(-4.0, 0.0, 0.0));
-    a.append_translation(&Translation3::new(0.0, 0.0, 0.0));
-
     window.set_light(Light::StickToCamera);
 
-    let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
-
-    while window.render() {
-        c.append_rotation_wrt_center(&rot);
-        s.append_rotation_wrt_center(&rot);
-        p.append_rotation_wrt_center(&rot);
-        y.append_rotation_wrt_center(&rot);
-        a.append_rotation_wrt_center(&rot);
-    }
+    while window.render() {}
 }
