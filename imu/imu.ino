0a6b7b998302920574859ec2d1ac60bb38d43c2b
Pixelprizm <pixelprizm@gmail.com>
2024-07-15 21:02:33 -0700

    [imu] Remove SPI and UART commented out code, some cleanup

--- imu/imu.ino
+++ imu/imu.ino
@@ -1,37 +1,27 @@
-// Basic demo for readings from Adafruit BNO08x
 #include <Adafruit_BNO08x.h>
 
-// For SPI mode, we need a CS pin
-#define BNO08X_CS 10
-#define BNO08X_INT 9
-
-// For SPI mode, we also need a RESET
-// #define BNO08X_RESET 5
-// but not for I2C or UART
 #define BNO08X_RESET -1
 
 Adafruit_BNO08x bno08x(BNO08X_RESET);
-sh2_SensorValue_t sensorValue;
+sh2_SensorValue_t bnoSensorValue;
 
 void setup(void)
 {
   Serial.begin(115200);
   while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+    delay(10);
 
-  Serial.println("Adafruit BNO08x test!");
+  Serial.println("Light Curve IMU");
 
-  // Try to initialize!
   if (!bno08x.begin_I2C())
   {
-    // if (!bno08x.begin_UART(&Serial1)) {  // Requires a device with > 300 byte UART buffer!
-    // if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_INT)) {
     Serial.println("Failed to find BNO08x chip");
     while (1)
     {
       delay(10);
     }
   }
+
   Serial.println("BNO08x Found!");
 
   for (int n = 0; n < bno08x.prodIds.numEntries; n++)
@@ -48,14 +38,13 @@ void setup(void)
     Serial.println(bno08x.prodIds.entry[n].swBuildNumber);
   }
 
-  setReports();
+  tellBnoWhatReportsWeWant();
 
   Serial.println("Reading events");
   delay(100);
 }
 
-// Here is where you define the sensor outputs you want to receive
-void setReports(void)
+void tellBnoWhatReportsWeWant(void)
 {
   Serial.println("Setting desired reports");
   if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR))
@@ -70,27 +59,26 @@ void loop()
 
   if (bno08x.wasReset())
   {
-    Serial.print("sensor was reset ");
-    setReports();
+    Serial.print("Sensor was reset - ");
+    tellBnoWhatReportsWeWant();
   }
 
-  if (!bno08x.getSensorEvent(&sensorValue))
+  if (!bno08x.getSensorEvent(&bnoSensorValue))
   {
     return;
   }
 
-  switch (sensorValue.sensorId)
+  switch (bnoSensorValue.sensorId)
   {
-
   case SH2_GAME_ROTATION_VECTOR:
     Serial.print("Game Rotation Vector - r: ");
-    Serial.print(sensorValue.un.gameRotationVector.real);
+    Serial.print(bnoSensorValue.un.gameRotationVector.real);
     Serial.print(" i: ");
-    Serial.print(sensorValue.un.gameRotationVector.i);
+    Serial.print(bnoSensorValue.un.gameRotationVector.i);
     Serial.print(" j: ");
-    Serial.print(sensorValue.un.gameRotationVector.j);
+    Serial.print(bnoSensorValue.un.gameRotationVector.j);
     Serial.print(" k: ");
-    Serial.println(sensorValue.un.gameRotationVector.k);
+    Serial.println(bnoSensorValue.un.gameRotationVector.k);
     break;
   }
 }
